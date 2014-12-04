fddee2b45a07e5097466e1fa147b391d95efe80e
Daniel Coorey <daniel.coorey@gmail.com>
2014-12-04 16:15:18 +1000

    Implement first 4 tests

--- HangmanTests/GameTest.cs
+++ HangmanTests/GameTest.cs
@@ -12,10 +12,19 @@ namespace HangmanTests
             Game game = new Game("Dinosaur", 5, '*');            
         }
 
+        [TestMethod]
         public void RevealedStateShouldShowMaskCharacterForUnknownLetters()
         {
             Game game = new Game("Tree", 5, '*');
             Assert.AreEqual("****", game.RevealedState);
+        }
+
+        [TestMethod]
+        public void RevealedStateShouldShowMaskCharacterForUnknownLetters2()
+        {
+            Game game = new Game("Tree", 5, '*');
+            game.GuessLetter('e');
+            Assert.AreEqual("**ee", game.RevealedState);
         }
 
         [TestMethod]
@@ -27,96 +36,124 @@ namespace HangmanTests
             Assert.IsFalse(game.GuessLetter('z'));
             Assert.AreEqual(4, game.RemainingAttempts);
         }
-        
+
         [TestMethod]
         public void GuessingCorrectLetterShouldUpdateRevealedState()
         {
             Game game = new Game("Bubble", 5, '*');
-            Assert.IsFalse(game.GuessLetter('b'));
+            Assert.IsTrue(game.GuessLetter('b'));
             Assert.AreEqual("B*bb**", game.RevealedState);
         }
 
-        [TestMethod]
-        public void GuessingCorrectLetterShouldNotDecrementRemainingAttempts()
-        {
-            Game game = new Game("Dinosaur", 5, '*');
-            Assert.AreEqual(5, game.RemainingAttempts);
-            Assert.IsTrue(game.GuessLetter('d'));
-            Assert.AreEqual(5, game.RemainingAttempts);
-        }
+        //[TestMethod]
+        //public void GuessingCorrectLetterShouldNotDecrementRemainingAttempts()
+        //{
+        //    Game game = new Game("Dinosaur", 5, '*');
+        //    Assert.AreEqual(5, game.RemainingAttempts);
+        //    Assert.IsTrue(game.GuessLetter('d'));
+        //    Assert.AreEqual(5, game.RemainingAttempts);
+        //}
 
-        [TestMethod]
-        public void GuessingWordCorrectlyShouldRevealWord()
-        {
-            Game game = new Game("Brother", 5, '*');
-            Assert.IsTrue(game.Guess("Brother"));
-            Assert.AreEqual("Brother", game.RevealedState);
-        }
+        //[TestMethod]
+        //public void GuessingWordCorrectlyShouldRevealWord()
+        //{
+        //    Game game = new Game("Brother", 5, '*');
+        //    Assert.IsTrue(game.Guess("Brother"));
+        //    Assert.AreEqual("Brother", game.RevealedState);
+        //}
 
-        [TestMethod]
-        public void GuessingWordCorrectlyShouldScoreNumberOfLettersInWordMultipliedByRemainingAttempts()
-        {
-            Game game = new Game("Brother", 5, '*');
-            game.GuessLetter('b');
-            game.GuessLetter('r');            
-            game.Guess("Brother");
-            Assert.AreEqual(21, game.Score);
-        }
+        //[TestMethod]
+        //public void GuessingWordCorrectlyShouldScoreNumberOfLettersInWordMultipliedByRemainingAttempts()
+        //{
+        //    Game game = new Game("Brother", 5, '*');
+        //    game.GuessLetter('b');
+        //    game.GuessLetter('r');            
+        //    game.Guess("Brother");
+        //    Assert.AreEqual(21, game.Score);
+        //}
 
-        [TestMethod]
-        public void RunningOutOfAttemptsShouldScoreNegativeNumberOfUnrevealedLettersMultipliedByAllowedAttempts()
-        {
-            Game game = new Game("Brother", 5, '*');
-            //Correct guesses
-            game.GuessLetter('b');
-            game.GuessLetter('r');
+        //[TestMethod]
+        //public void RunningOutOfAttemptsShouldScoreNegativeNumberOfUnrevealedLettersMultipliedByAllowedAttempts()
+        //{
+        //    Game game = new Game("Brother", 5, '*');
+        //    //Correct guesses
+        //    game.GuessLetter('b');
+        //    game.GuessLetter('r');
 
-            //Incorrect guesses
-            game.Guess("braille");
-            game.GuessLetter('a');
-            game.GuessLetter('u');
-            game.GuessLetter('i');
-            game.GuessLetter('d');            
-            Assert.AreEqual(-25, game.Score);
-        }
+        //    //Incorrect guesses
+        //    game.Guess("braille");
+        //    game.GuessLetter('a');
+        //    game.GuessLetter('u');
+        //    game.GuessLetter('i');
+        //    game.GuessLetter('d');            
+        //    Assert.AreEqual(-25, game.Score);
+        //}
 
-        [TestMethod]
-        public void GameIsOverWhenNoMoreRemainingAttemptsOrAllLettersAreRevealed() {
-            Game losingGame = new Game("Brother", 3, '*');
-            losingGame.Guess("braille");
-            losingGame.GuessLetter('a');
-            losingGame.GuessLetter('u');
-            Assert.IsTrue(losingGame.IsOver);
+        //[TestMethod]
+        //public void GameIsOverWhenNoMoreRemainingAttemptsOrAllLettersAreRevealed() {
+        //    Game losingGame = new Game("Brother", 3, '*');
+        //    losingGame.Guess("braille");
+        //    losingGame.GuessLetter('a');
+        //    losingGame.GuessLetter('u');
+        //    Assert.IsTrue(losingGame.IsOver);
 
-            Game winningGame = new Game("Bubble", 3, '*');
-            winningGame.Guess("bubble");
-            Assert.IsTrue(winningGame.IsOver);
-        }
+        //    Game winningGame = new Game("Bubble", 3, '*');
+        //    winningGame.Guess("bubble");
+        //    Assert.IsTrue(winningGame.IsOver);
+        //}
     }
+
+
 
     ////Auto-generated game class with no implementation
     //class Game
     //{
+    //    String word;
+
     //    public Game(String word, int allowedAttempts, char letterMask)
     //    {
-
+    //        this.word = word;
+    //        RevealedState = String.Empty;
+    //        for (int i = 0; i < word.Length; i++)
+    //        {
+    //            RevealedState += letterMask;
+    //        }
+    //        RemainingAttempts = allowedAttempts;
+                
     //    }
 
-    //    public bool GuessLetter(char p)
+    //    public bool GuessLetter(char guess)
     //    {
-    //        throw new NotImplementedException();
+    //        char[] revealed = RevealedState.ToCharArray();
+    //        Boolean foundGuessInWord = false;
+
+    //        for (int i = 0; i < word.Length; i++)
+    //        {
+    //            if (word[i] == guess) {
+    //                revealed[i] = guess;
+    //                foundGuessInWord = true;
+    //            }
+    //        }
+    //        if (!foundGuessInWord)
+    //        {
+    //            RemainingAttempts -= 1;
+    //        }
+
+    //        RevealedState = new String(revealed);
+
+    //        return foundGuessInWord;
     //    }
 
     //    public int RemainingAttempts { get; set; }
 
-    //    public String RevealedState { get; set; }
+    //    public String RevealedState { get; private set; }
 
-    //    public bool Guess(string p)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+    //    //public bool Guess(string p)
+    //    //{
+    //    //    throw new NotImplementedException();
+    //    //}
 
-    //    public int Score { get; set; }
-    //    public bool IsOver { get; set; }
+    //    //public int Score { get; set; }
+    //    //public bool IsOver { get; set; }
     //}
 }
--- HangmanWinForms/MainForm.cs
+++ HangmanWinForms/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,48 @@ namespace HangmanWinForms
 {
     public partial class MainForm : Form
     {
+        Game currentGame;
+        String[] nouns = File.ReadAllLines("nouns.txt");
+        Random rnd = new Random();
+        readonly int MAX_GUESSES = 5;
+        readonly char MASK_CHAR = '*';
+            
         public MainForm()
         {
             InitializeComponent();
-
         }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {            
+            var word = nouns[rnd.Next(nouns.Length)];
+            currentGame = new Game(word, MAX_GUESSES, MASK_CHAR);
+            SyncFrom(currentGame);
+        }
+
+        private void SyncFrom(Game currentGame)
+        {
+            remainingGuessesLabel.Text = currentGame.RemainingAttempts;
+            revealedLabel.Text = currentGame.RevealedState;
+            if (currentGame.IsOver)
+            {
+                scoreLabel.Text = currentGame.Score;
+            }
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            var guess = guessTextBox.Text;
+            if (guess.Length == 1)
+            {
+                currentGame.GuessLetter(guess[0]);
+            }
+            else if (guess.Length > 1)
+            {
+                currentGame.Guess(guess);
+            }
+            guessTextBox.Text = String.Empty;
+            SyncFrom(currentGame);
+        }
+
     }
 }
